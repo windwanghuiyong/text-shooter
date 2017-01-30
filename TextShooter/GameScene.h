47c282d3133f502c9564d1545e8589961e96b8de
王慧勇 <weifeng19911024@163.com>
2017-01-30 14:45:20 +0800

    Initial Commit

---
+++ TextShooter/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  TextShooter
+//
+//  Created by wanghuiyong on 30/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ TextShooter/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  TextShooter
+//
+//  Created by wanghuiyong on 30/01/2017.
+//  Copyright © 2017 Personal Organization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
